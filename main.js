5185960b15a8bc700ed9258c68da19cb9832ecd0
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-09 15:34:58 -0400

    updated layout made buttons 3D

---
+++ main.js
@@ -0,0 +1,11 @@
+
+document.querySelectorAll('button').forEach(button =>{
+    pushNumber.addEventListener('click', () =>{
+      pushNumber(el.textContent);
+    });
+  });
+
+
+function pushNumber (push) {
+    
+}
