94aceeac20b9f3196b76ef77c295d7d95141eea5
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-11 17:13:08 -0400

    added functionality to operators

--- main.js
+++ main.js
@@ -12,19 +12,49 @@
     }
 
     pushNumber(number) {
-      this.currentOperand = number
-    }
+      if (number === '.' && this.currentOperand.includes('.')) return
+      this.currentOperand = this.currentOperand.toString() + number.toString()
+     }
 
     pushOperator(operation) {
-
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') {
+        this.calculate()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
     }
 
     calculate() {
-
+      let calculate 
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch(this.operation) {
+        case '+':
+          calculate = prev + current
+          break
+        case '-':
+          calculate = prev - current
+          break
+        case '*':
+            calculate = prev * current
+            break
+        case '/':
+            calculate = prev / current
+            break
+        default:
+          return
+      }
+      this.currentOperand = calculate
+      this.operation = undefined
+      this.previousOperand = ''
     }
 
     updateScreen () {
       this.currentOperandTextElement.innerText = this.currentOperand
+      this.previousOperandTextElement.innerText = this.previousOperand
     }
 
 
@@ -54,4 +84,21 @@ numberButtons.forEach(button => {
     calculator.pushNumber(button.innerText)
     calculator.updateScreen()
   })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.pushOperator(button.innerText)
+    calculator.updateScreen()
+  })
+})
+
+equalsButton.addEventListener('click', button  => {
+  calculator.calculate()
+  calculator.updateScreen()
+})
+
+allClearButton.addEventListener('click', button  => {
+  calculator.allClear()
+  calculator.updateScreen()
 })
